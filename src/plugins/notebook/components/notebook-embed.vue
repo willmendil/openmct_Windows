3cd8612b16f0c513affe9c7ed92d812c38af7775
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2020-04-06 13:58:05 -0700

    Added removeDialog class.

--- src/plugins/notebook/components/notebook-embed.vue
+++ src/plugins/notebook/components/notebook-embed.vue
@@ -29,6 +29,7 @@ import PopupMenu from './popup-menu.vue';
 import PreviewAction from '../../../ui/preview/PreviewAction';
 import RemoveDialog from '../utils/removeDialog';
 import PainterroInstance from '../utils/painterroInstance';
+import RemoveDialog from '../utils/removeDialog';
 import SnapshotTemplate from './snapshot-template.html';
 import Vue from 'vue';
 
@@ -63,6 +64,9 @@ export default {
     },
     mounted() {
         this.addPopupMenuItems();
+    },
+    mounted() {
+        this.initRemoveDialog();
     },
     methods: {
         addPopupMenuItems() {
